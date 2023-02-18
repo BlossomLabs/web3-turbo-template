f463cd213286474ce3a57affa33d63fff8c4a25e
Viviane Dias <vivianedias@pm.me>
2023-02-18 11:11:36 -0300

    test: fix import for btn

--- pkg/ui/src/__tests__/Button.test.tsx
+++ pkg/ui/src/__tests__/Button.test.tsx
@@ -1,6 +1,6 @@
 import { expect, test } from "vitest";
 import { render, screen } from "@testing-library/react";
-import Button from "../Button";
+import Button from "../components/Button";
 
 test("Button", () => {
   render(<Button />);
