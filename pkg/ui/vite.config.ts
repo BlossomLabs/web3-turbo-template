24fcc1e7d2c74fc1b68343442d766619d9568147
Viviane Dias <vivianedias@pm.me>
2023-02-24 10:23:59 -0300

    chore(vite): adds vite plugin dts to plugins in config

--- pkg/ui/vite.config.ts
+++ pkg/ui/vite.config.ts
@@ -1,10 +1,11 @@
 import { resolve } from "path";
 import { defineConfig } from "vitest/config";
 import react from "@vitejs/plugin-react-swc";
+import dts from "vite-plugin-dts";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), dts()],
   build: {
     lib: {
       // Could also be a dictionary or array of multiple entry points
